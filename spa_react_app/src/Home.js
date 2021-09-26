4b6e1f3fee884064c6d072393bc8bc9cc7d891f8
deshpandem10 <deshpandem10@gmail.com>
2021-09-26 11:55:03 +0400

    Pages created, basic routing developed using HashRouter, NavLink and Route from react-router-dom

---
+++ spa_react_app/src/Home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Home page</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros sapien, laoreet nec ligula quis, gravida accumsan nulla. 
+                    Vivamus aliquam porta viverra. Duis mattis mi non sem cursus faucibus. Sed nec nunc eget purus dignissim commodo a at turpis. 
+                    Nullam sodales est ut eros rhoncus, ut volutpat sapien dapibus. Nunc fringilla facilisis urna, a aliquet enim consequat vel. 
+                </p>
+                <p>
+                    Aliquam vel scelerisque est. Maecenas blandit tellus sed risus pretium viverra. Vestibu
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
