5ada9ba02ef206b495247e0cfdd0468b91b15dad
deshpandem10 <deshpandem10@gmail.com>
2021-09-26 14:46:31 +0400

    css for navigation bar in progress

--- spa_react_app/src/Main.js
+++ spa_react_app/src/Main.js
@@ -9,13 +9,15 @@ import Home from './Home';
 import Stuff from './Stuff';
 import Contact from './Contact';
 
+import './scss/navbar.scss';
+
 class Main extends Component {
     render() {
       return (
         <HashRouter>
             <div>
                 <h1>Simple SPA</h1>
-                <ul>
+                <ul class="navbar">
                     <li>
                         <NavLink to="/">Home</NavLink>
                     </li>
@@ -28,7 +30,7 @@ class Main extends Component {
                 </ul>
                 
                 <div className="content">
-                    <Route path="/" component={Home}/>
+                    <Route exact path="/" component={Home}/>
                     <Route path="/stuff" component={Stuff}/>
                     <Route path="/contact" component={Contact}/>
                 </div>
@@ -36,6 +38,6 @@ class Main extends Component {
         </HashRouter>
       );
     }
-  }
+}
 
 export default Main;
