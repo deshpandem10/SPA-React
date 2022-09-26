de1ac86ac77fbc59fb3b4d1c7c369a3358b3de1e
madhura <deshpandem10@gmail.com>
2022-09-26 15:15:54 +0400

    Contact page content update, anchor tag underline animation

--- spa_react_app/src/contents/Contact.js
+++ spa_react_app/src/contents/Contact.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 
+import Social from '../components/Social';
+
 class Contact extends Component {
     render () {
         return (
             <div className="container">
-                <h1>This is Contact page</h1>
+                <h1 className='pagehead'>Reach Out to Me!</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros sapien, laoreet nec ligula quis, gravida accumsan nulla. 
-                    Vivamus aliquam porta viverra. Duis mattis mi non sem cursus faucibus. Sed nec nunc eget purus dignissim commodo a at turpis. 
-                    Nullam sodales est ut eros rhoncus, ut volutpat sapien dapibus. Nunc fringilla facilisis urna, a aliquet enim consequat vel. 
+                    Whether you want to get in touch, discuss projects, or just say hi, I'd love you hear from you {String.fromCodePoint('0x1f60A')}
                 </p>
-                <p>
-                    Aliquam vel scelerisque est. Maecenas blandit tellus sed risus pretium viverra. Vestibu
-                </p>
+
+                <div className="sendMail">
+                    <a href="mailto: deshpandem0@gmail.com">
+                        <i className='bx bx-envelope' ></i> deshpandem10@gmail.com
+                    </a>
+                    <div>
+                        <i className='bx bx-map'></i> Dubai, United Arab Emirates
+                    </div>
+                </div>
+                <Social/>
             </div>
         );
     }
