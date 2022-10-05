05fc2a2afd6d36cfa99484551be7dc6f9d464d19
madhura <deshpandem10@gmail.com>
2022-10-05 18:39:16 +0400

    content added for work experience page

--- spa_react_app/src/components/WideCard.js
+++ spa_react_app/src/components/WideCard.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 
-import  { Card, Button } from 'react-bootstrap';
+import  { 
+    Card,
+    Row,
+    Col
+} from 'react-bootstrap';
 
-class WideCard extends Component {
-    render () {
-        function handleCardClick() {
-            console.log('you clicked a card');
-        }
+import '../assets/scss/card.scss';
 
-        return (
-            <Card className="mini-card" onClick={handleCardClick}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-        )
-    }
+const WideCard = (props) => {
+    return (
+        <Card className="wideCard">
+            <Card.Body>
+                <Row>
+                    <Col lg={8} className='order-2 order-sm-1'>
+                        <p>
+                            {props.text}
+                        </p>
+                    </Col>
+                    <Col className='order-1 order-sm-2'>
+                        <div className='imgwrp'>
+                            <img src={props.image} className='img-fluid' />
+                        </div>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    )
 }
 
 export default WideCard;
--- spa_react_app/src/contents/Experience.js
+++ spa_react_app/src/contents/Experience.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import WideCard from '../components/WideCard';
+
+import precise_logo from '../assets/resources/logos/precise_logo.png';
+import inaxus_logo from '../assets/resources/logos/inaxus_logo.jpg';
+import cloudchain_logo from '../assets/resources/logos/cloudchain_logo.jpg';
+import kpit_logo from '../assets/resources/logos/kpit_logo.png';
+
 class Experience extends Component {
     render () {
         return (
-            <h1>experience</h1>
+            <Container>
+                <div>
+                    <h1 className='pagesubhead'>Work Experience</h1>
+
+                    <WideCard 
+                        image={precise_logo} 
+                        text={[
+                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit luctus posuere. 
+                            Nulla nec turpis velit. Nunc eu scelerisque ante.`, <br/>, 
+                            `Donec ut tortor nec elit pulvinar suscipit. 
+                            Donec tincidunt nunc lacus, a pretium leo pellentesque vitae. Pellentesque habitant morbi 
+                            tristique senectus et netus et malesuada fames ac turpi`
+                        ]}
+                    />
+                    <WideCard 
+                        image={inaxus_logo} 
+                        text={[
+                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit luctus posuere. 
+                            Nulla nec turpis velit. Nunc eu scelerisque ante.`, <br/>, 
+                            `Donec ut tortor nec elit pulvinar suscipit. 
+                            Donec tincidunt nunc lacus, a pretium leo pellentesque vitae. Pellentesque habitant morbi 
+                            tristique senectus et netus et malesuada fames ac turpi`
+                        ]}
+                    />
+                    <WideCard 
+                        image={cloudchain_logo} 
+                        text={[
+                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit luctus posuere. 
+                            Nulla nec turpis velit. Nunc eu scelerisque ante.`, <br/>, 
+                            `Donec ut tortor nec elit pulvinar suscipit. 
+                            Donec tincidunt nunc lacus, a pretium leo pellentesque vitae. Pellentesque habitant morbi 
+                            tristique senectus et netus et malesuada fames ac turpi`
+                        ]}
+                    />
+                    <WideCard 
+                        image={kpit_logo} 
+                        text={[
+                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit luctus posuere. 
+                            Nulla nec turpis velit. Nunc eu scelerisque ante.`, <br/>, 
+                            `Donec ut tortor nec elit pulvinar suscipit. 
+                            Donec tincidunt nunc lacus, a pretium leo pellentesque vitae. Pellentesque habitant morbi 
+                            tristique senectus et netus et malesuada fames ac turpi`
+                        ]}
+                    />
+                </div>
+            </Container>
         )
     }
 }
